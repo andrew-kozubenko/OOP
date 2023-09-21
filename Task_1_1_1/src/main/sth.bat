11793ba0ba00ffadf21a60425e34d36d1ebdba76
andrew-kozubenko <a.kozubenko@g.nsu.ru>
2023-09-21 16:36:33 +0700

    tyt

--- Task_1_1_1/src/main/sth.bat
+++
@@ -1,5 +0,0 @@
-javac -d \out .\java\org\example\Main.java .\java\org\example\HeapSort.java
-javadoc -d \out .\javadoc .\java\org\example\Main.java .\java\org\example\HeapSort.java
-cd .\out
-java -classpath . org.example.Main
-pause
