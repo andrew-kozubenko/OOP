dce39ac82dcfc4a80c927f024cfd79589f91c1fa
andrew-kozubenko <a.kozubenko@g.nsu.ru>
2023-09-21 23:22:15 +0700

    Yes

--- Task_1_1_1/src/main/sth.bat
+++ Task_1_1_1/src/main/sth.bat
@@ -2,4 +2,3 @@ javac -d \out .\java\org\example\Main.java .\java\org\example\HeapSort.java
 javadoc -d \out .\javadoc .\java\org\example\Main.java .\java\org\example\HeapSort.java
 cd .\out
 java -classpath . org.example.Main
-pause
