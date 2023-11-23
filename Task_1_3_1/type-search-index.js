1caeca357ffa2fc4e1dd1b7d5f24f40e9f348117
andrew-kozubenko <andrew-kozubenko@users.noreply.github.com>
2023-11-23 03:08:04 +0000

    Deploying to gh-pages from @ andrew-kozubenko/OOP@469aa86ab7a2a2535f260d5976b37a1b363ec7f5 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"SubstringSearch"},{"p":"org.example","l":"TestUtils"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"SubstringSearch"}]
