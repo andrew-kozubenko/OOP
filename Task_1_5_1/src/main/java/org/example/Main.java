a52d912c9e4bf83a08d14fe1d34b91cc3191985e
Andrew Kozubenko <a.kozubenko@g.nsu.ru>
2023-12-27 15:21:35 +0700

    Test

--- Task_1_5_1/src/main/java/org/example/Main.java
+++ Task_1_5_1/src/main/java/org/example/Main.java
@@ -10,25 +10,25 @@ public class Main {
      * main.
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите выражение в префиксной форме:");
-        String expression = scanner.nextLine();
-
-        try {
-            double result = Calculator.evaluateExpressionDouble(expression);
-            System.out.println("Результат: " + result);
-        } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-
-        System.out.println("Введите комплексное выражение в префиксной форме:");
-        String expression1 = scanner.nextLine();
-
-        try {
-            ComplexNumber result1 = Calculator.evaluateExpressionComplex(expression1);
-            System.out.println("Результат: " + result1.toString());
-        } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите выражение в префиксной форме:");
+//        String expression = scanner.nextLine();
+//
+//        try {
+//            double result = Calculator.evaluateExpressionDouble(expression);
+//            System.out.println("Результат: " + result);
+//        } catch (Exception e) {
+//            System.out.println("Ошибка: " + e.getMessage());
+//        }
+//
+//        System.out.println("Введите комплексное выражение в префиксной форме:");
+//        String expression1 = scanner.nextLine();
+//
+//        try {
+//            ComplexNumber result1 = Calculator.evaluateExpressionComplex(expression1);
+//            System.out.println("Результат: " + result1.toString());
+//        } catch (Exception e) {
+//            System.out.println("Ошибка: " + e.getMessage());
+//        }
     }
 }
